01a054c5de8d6e9fd0dbb5a023e2d3fc16ee8f72
antipers <antipers@gmail.com>
2022-07-25 01:09:07 +0300

    completed

---
+++ task64/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число M: ");
+int valueM = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число N: ");
+int valueN = int.Parse(Console.ReadLine()!);
+
+
+int PeriodicSum(int FirstNum, int SecondNum)
+{
+    if (FirstNum == SecondNum) return SecondNum;
+    else return (FirstNum+ (PeriodicSum(FirstNum + 1, SecondNum)));
+}
+
+System.Console.WriteLine($"сумма натуральных элементов в промежутке от M={valueM} до N={valueN} равняется {PeriodicSum(valueM, valueN)}");
+
+
+/* метод для решения без рекурсии циклом 
+int PeriodicSum(int valueM, int valueN)
+{
+    int sum = 0;
+    {
+        for (int i = valueM; i <= valueN; i++)
+        {
+            sum += i;
+        }
+        if (valueM == valueN)
+        {
+            sum += valueN;
+        }
+    }
+    return sum;
+} */
---
+++ task66/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 66: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+
+// A(n, m) = m + 1, если n = 0,
+// = A(n - 1, 1), если n <> 0, m = 0,
+// = A(n - 1, A(n, m - 1)), если n> 0, m > 0.
+
+Console.WriteLine("Введите число M: ");
+int valueN = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число N: ");
+int valueM = int.Parse(Console.ReadLine()!);
+
+int Ackermann(int valueM,int valueN)
+{
+    if (valueM == 0)
+    {
+        return valueN+1;
+    }
+    if (valueN == 0 && valueM > 0)
+    {
+        return Ackermann(valueM - 1, 1);
+    }
+    if (valueN > 0 && valueM > 0)
+    {
+        return Ackermann((valueM - 1), Ackermann(valueM, (valueN - 1)));
+    }
+    else return 0;
+}
+
+int result = Ackermann(valueN, valueM);
+System.Console.WriteLine(result);
